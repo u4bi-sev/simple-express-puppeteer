a5db04aaf22e279f607bc91dd06962fb31211339
u4bi <admin@naver.id>
2018-06-19 01:44:45 +0900

    Added express-async-handler to package.json

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,36 @@
 const express = require('express'),
+      asyncHandler = require('express-async-handler'),
       router = express.Router();
 
 router.get('/', (req, res, next) => res.json({
     test : 'u4bi'
+}));
+
+router.get('/async', asyncHandler(async (req, res, next) => {
+
+    let wait = ms => new Promise(r => setTimeout(r, ms)),
+        arr = [];
+    
+    await wait(1000);
+    await arr.push('text 1');
+
+    await wait(1000);
+    await arr.push('text 2');
+
+    await wait(1000);
+    await arr.push('text 3');
+
+    await wait(1000);
+    await arr.push('text 4');
+
+    await wait(1000);
+    await arr.push('text 5');
+
+    res.json({
+        test : 'async',
+        data : arr
+    });
+
 }));
 
 module.exports = router;
