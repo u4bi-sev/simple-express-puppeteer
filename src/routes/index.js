290be623c67b57289fd7f6d2a93df215ea749f88
u4bi <admin@naver.id>
2018-06-19 02:17:27 +0900

    Added puppeteer to package.json

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express'),
       asyncHandler = require('express-async-handler'),
       router = express.Router();
 
+const puppeteer = require('./puppeteer');
+
+
 router.get('/', (req, res, next) => res.json({
     test : 'u4bi'
 }));
@@ -17,20 +20,13 @@ router.get('/async', asyncHandler(async (req, res, next) => {
     await wait(1000);
     await arr.push('text 2');
 
-    await wait(1000);
-    await arr.push('text 3');
-
-    await wait(1000);
-    await arr.push('text 4');
-
-    await wait(1000);
-    await arr.push('text 5');
-
     res.json({
         test : 'async',
         data : arr
     });
 
 }));
+
+router.get('/naver', asyncHandler(puppeteer));
 
 module.exports = router;
